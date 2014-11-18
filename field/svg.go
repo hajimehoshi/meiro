f5a0bcc9c13c86142b17da81d79167f7c8feb7ba
Hajime Hoshi <hajimehoshi@gmail.com>
2014-11-19 03:09:09 +0900

    Remove some nits of SVG

--- field/svg.go
+++ field/svg.go
@@ -10,9 +10,9 @@ import (
 const svgRoomSize = 8
 
 var svgTemplate = `
-<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<?xml version="1.0" encoding="utf-8" standalone="no"?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="0 0 {{width}} {{height}}" background-color="#fff">
+<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 {{width}} {{height}}" background-color="#fff">
 <g transform="translate({{offsetX}}, {{offsetY}})" stroke="black" stroke-width="1" stroke-linecap="round">
 {{lines}}
 </g>
